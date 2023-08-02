dc08cb06f8f32f1a0138ec152fe285d80bef9ea0
dobalissimo <dobalissimo@gmail.com>
2023-08-02 20:48:34 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+let sonidoBatman = document.querySelector(".bati_encendido")
+let sonidoBoton = document.querySelector(".bati_click")
+
+let batman = document.querySelector(".batman");
+let batmanBoton = document.querySelector(".bati_boton");
+
+let estadoLinterna = "apagado"
+
+//eventos
+
+
+
+// funciones
+function controlarLinterna(){
+    if (estadoLinterna=="apagado") {
+        estadoLinterna="encendido";
+        sonidoLinterna()
+        batman.classList.add("batman-activo")
+
+    }else{ 
+        estadoLinterna="apagado"
+        sonidoLinterna()
+        batman.classList.remove("batman-activo")
+
+    }
+}
+
+function sonidoLinterna(){
+    if(sonidoBatman.paused){
+        sonidoBoton.play();
+        sonidoBatman.play();
+    }else{
+        sonidoBoton.play();
+        sonidoBatman.pause();    }
+}
